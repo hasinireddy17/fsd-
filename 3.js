c206a7195b1fc721aca0a6cab0793e29e00f4ba4
Hasini Reddy <hasinireddy@Hasinis-MacBook-Air.local>
2026-05-25 14:48:35 +0530

    Commit

---
+++ 3.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("buyBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+    message.innerText = " Thanks for choosing SmartWatch Pro!";
+});
